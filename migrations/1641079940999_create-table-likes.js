73a3ee5789123c97c5a575d7b8072365f0c7c1c1
mnfaizp <zynthkas@gmail.com>
2022-01-02 08:31:32 +0700

    fix little in migrations

--- migrations/1641079940999_create-table-likes.js
+++ migrations/1641079940999_create-table-likes.js
@@ -17,7 +17,7 @@ exports.up = (pgm) => {
   });
 
   pgm.addConstraint('likes', 'fk_likes.comment_id_comments.id', 'FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE');
-  pgm.addConstraint('likes', 'fk_likes.owner_users.id', 'FOREIGN KEY (owner) REFERENCES users`(id) ON DELETE CASCADE');
+  pgm.addConstraint('likes', 'fk_likes.owner_users.id', 'FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
