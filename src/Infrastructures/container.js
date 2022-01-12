fd872e6507139aa47347d832bf4b8f6378cf81b7
mnfaizp <zynthkas@gmail.com>
2022-01-12 18:26:08 +0700

    like feature come again

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -22,6 +22,8 @@ const CommentRepository = require('../Domains/comments/CommentRepository');
 const CommentRepositoryPostgres = require('./repository/CommentRepositoryPostgres');
 const ReplyRepository = require('../Domains/replies/ReplyRepository');
 const ReplyRepositoryPostgres = require('./repository/ReplyRepositoryPostgres');
+const LikeRepository = require('../Domains/likes/LikeRepository');
+const LikeRepositoryPostgres = require('./repository/LikeRepositoryPostgres');
 
 // use case
 const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
@@ -38,6 +40,7 @@ const DeleteCommentUseCase = require('../Applications/use_case/DeleteCommentUseC
 const GetThreadCommentsUseCase = require('../Applications/use_case/GetThreadCommentsUseCase');
 const AddReplyUseCase = require('../Applications/use_case/AddReplyUseCase');
 const DeleteReplyUseCase = require('../Applications/use_case/DeleteReplyUseCase');
+const LikeUnlikeCommentUseCase = require('../Applications/use_case/LikeUnlikeCommentUseCase');
 
 // crea../Applications/use_case/ting container
 const container = createContainer();
@@ -156,10 +159,45 @@ container.register([
       ],
     },
   },
+  {
+    key: LikeRepository.name,
+    Class: LikeRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        {
+          concrete: pool,
+        },
+        {
+          concrete: nanoid,
+        },
+      ],
+    },
+  },
 ]);
 
 // registering use cases
 container.register([
+  {
+    key: LikeUnlikeCommentUseCase.name,
+    Class: LikeUnlikeCommentUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        {
+          name: 'commentRepository',
+          internal: CommentRepository.name,
+        },
+        {
+          name: 'threadRepository',
+          internal: ThreadRepository.name,
+        },
+        {
+          name: 'likeRepository',
+          internal: LikeRepository.name,
+        },
+      ],
+    },
+  },
   {
     key: DeleteReplyUseCase.name,
     Class: DeleteReplyUseCase,
---
+++ src/Infrastructures/repository/LikeRepositoryPostgres.js
@@ -0,0 +1,57 @@
+const LikeRepository = require('../../Domains/likes/LikeRepository');
+
+class LikeRepositoryPostgres extends LikeRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._idGenerator = idGenerator;
+    this._pool = pool;
+  }
+
+  async checkOwnerLikeOnComments({ owner, commentId }) {
+    const query = {
+      text: 'SELECT id FROM likes WHERE owner = $1 AND comment_id = $2',
+      values: [owner, commentId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows;
+  }
+
+  async addLike({ commentId, owner }) {
+    const id = `like-${this._idGenerator()}`;
+    const query = {
+      text: 'INSERT INTO likes(id, comment_id, owner) VALUES ($1, $2, $3)',
+      values: [id, commentId, owner],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async deleteLike({ owner, commentId }) {
+    const query = {
+      text: 'DELETE FROM likes WHERE comment_id = $1 AND owner = $2',
+      values: [commentId, owner],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async getLikesByThreadId({ threadId }) {
+    const query = {
+      text: 'SELECT count(likes.id) as likes, comments.id FROM threads JOIN comments ON comments.thread_id = threads.id JOIN likes ON likes.comment_id = comments.id WHERE threads.id = $1 GROUP BY comments.id',
+      values: [threadId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows.map((likes) => {
+      const newComment = {};
+      newComment.likes = likes.likes;
+      newComment.commentId = likes.id;
+      return newComment;
+    });
+  }
+}
+
+module.exports = LikeRepositoryPostgres;
