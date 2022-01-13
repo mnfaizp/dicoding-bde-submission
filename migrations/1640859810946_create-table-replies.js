9119ff5f8ce6115706817283f12a4bea871b2122
mnfaizp <zynthkas@gmail.com>
2022-01-13 09:18:22 +0700

    add contraint to users table

--- migrations/1640859810946_create-table-replies.js
+++ migrations/1640859810946_create-table-replies.js
@@ -28,6 +28,7 @@ exports.up = (pgm) => {
   });
 
   pgm.addConstraint('replies', 'fk_replies.comment_id_comments.id', 'FOREIGN KEY (comment_id) REFERENCES comments(id) ON DELETE CASCADE');
+  pgm.addConstraint('replies', 'fk_replies.owner_users.id', 'FOREIGN KEY (owner) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
